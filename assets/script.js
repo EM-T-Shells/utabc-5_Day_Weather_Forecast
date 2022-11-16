902af94afec20c59a496252db8d13dc673abe67d
Gokhan Emre Taskale <taskaleg@gmail.com>
2022-11-16 14:35:35 -0600

    functional app, clear button function in progress. Will add function to redirecting user if clicking city in history list

--- assets/script.js
+++ assets/script.js
@@ -3,7 +3,7 @@ function getData()
 {
    var searchCity = document.getElementById('jsInput');
    var cityName = document.getElementById("jsCityName");
-   cityName.innerHTML = searchCity.value;
+   cityName.innerHTML = searchCity.value; //data return is connected to this variable
 
    fetch("https://api.openweathermap.org/data/2.5/forecast?q="+searchCity.value+"&units=imperial&appid=1d7c424a7e3f71fedc830945372a8502")
    .then (response => response.json())
@@ -12,8 +12,8 @@ function getData()
       for(i=0; i<5; i++)
       {
          document.getElementById("jsTemp" + (i+1)).innerText = "T: " + (data.list[i].main.temp + " °f");
-         document.getElementById("jsTempMax" + (i+1)).innerText = "H: " + (data.list[i].main.temp_max + " °f");
-         document.getElementById("jsTempMin" + (i+1)).innerText = "L: " + (data.list[i].main.temp_min + " °f");
+         document.getElementById("jsHumidity" + (i+1)).innerText = "Humidity: " + (data.list[i].main.humidity + " %");
+         document.getElementById("jsWindSpeed" + (i+1)).innerText = "Wind Speed: " + (data.list[i].wind.speed + " mph");
          document.getElementById("jsDescr" + (i+1)).innerText = data.list[i].weather[0].description;
          document.getElementById("jsIcon" + (i+1)).src = "http://openweathermap.org/img/wn/"+
          data.list[i].weather[0].icon + ".png";
@@ -36,12 +36,54 @@ for (let i=0; i<5; i++)
    document.getElementById("jsDay" + (i+1)).innerText = weekdays[checkDay(i)];
 }
 
+
+////////////////////////////////////////////////////////////////
+//local storage
+var searchCity = document.getElementById('jsInput');
+var cityName = document.getElementById("jsCityName");
+cityName.innerHTML = searchCity.value;
+
+var searchHistoryEl = document.getElementById('search-history');
+var searchClear = document.getElementById('search-clear');
+var jsBtn = document.getElementById("jsBtn");
+var searchHistory = JSON.parse(localStorage.getItem("city")) || [];
+
+
+jsBtn.addEventListener("click", (e) => {
+   e.preventDefault();
+   var city = jsInput.value.trim();
+   searchHistory.push(jsInput.value.trim());
+   localStorage.setItem("city",JSON.stringify(searchHistory));
+
+   getSearchHistory();
+      if (city)
+      {
+         citySearch(city)
+         jsInput.value = "";
+      }
+});
+
+function getSearchHistory(){
+   if (searchHistory !=""){
+   searchHistory = JSON.parse(localStorage.getItem("city"));
+   for (i = 0; i < searchHistory.length; i++){
+       var appendHistory = document.createElement("li")
+       appendHistory.innerHTML = searchHistory[i];
+       searchHistoryEl.appendChild(appendHistory);
+     }
+  }
+}
+
+////////////////////////////////////////////////////////////////
 //initial display
 function startScreen()
 {
    document.getElementById("jsInput").defaultValue = "Austin";
    getData();
-}; startScreen();
+};
+startScreen();
+
+
 
 //season changer
 
